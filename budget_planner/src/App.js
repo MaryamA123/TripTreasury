fe0eec598172d06432d6ae2fef64bc4df564393b
Maryam Asaria <maryam.asaria@gmail.com>
2023-11-29 11:12:17 +0000

    Created a React App

---
+++ budget_planner/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, I'm Maryam. <br/>
+          One of my favourite hobbies is playing Football <br/>
+          I enjoy it because I just love being active outside! <br/>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
