98b6fe83cd6f172572e105f3a97cfd1a142c8be4
AsiaEM <ayszhunna@gmail.com>
2023-12-01 11:16:01 +0000

    Asia About me

--- budget_planner/src/App.js
+++ budget_planner/src/App.js
@@ -18,7 +18,13 @@ function App() {
           Hiya, I'm Naomi. <br/>
           One of my favourite hobbies is pottery. <br/>
           I enjoy it because it is very calming and a good oppurtunity to be creative. <br/>
-      </p> 
+          </p>
+          <p>
+            Hello, I'm Asia. <br/>
+            One of my hobbies is skating. <br/>
+            I enjoy skating as is allows me to express myself through movement and music. <br/>
+          </p>
+      
       </header>
          
       </div>
