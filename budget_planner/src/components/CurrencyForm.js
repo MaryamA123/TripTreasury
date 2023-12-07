68c910cc8a07e3e013f1f1fbc3fa253548db7731
Penny <pennyluc321@gmail.com>
2023-12-07 17:01:06 +0000

    linked the frontend with the api, added a few more currencies, fixed the sidebar so it only appears when its clicked

--- budget_planner/src/components/CurrencyForm.js
+++ budget_planner/src/components/CurrencyForm.js
@@ -1,86 +1,131 @@
-import "./CurrencyForm.css"
-import React, {useState} from "react";
+import "./CurrencyForm.css";
+import React, { useState } from "react";
+import makeApiCall from "../services/api";
 
 const CurrencyForm = () => {
   const [formData, setFormData] = useState({
     currency1: "",
     currency2: "",
-    amount:0,
-    totalDays:0
-  })
-  
-  const [showDailyBudget, setShowDailyBudget] = useState(false);
+    amount: 0,
+    totalDays: 0,
+  });
 
-  const handleInputChange = (e) =>{
-    const {name, value} = e.target;
-    setFormData({...formData, [name]: name === "amount" || name === "totalDays" ? valueAsNumber(e): value})
-  }
+  const [showDailyBudget, setShowDailyBudget] = useState(false);
+  const [dailyBudget, setDailyBudget] = useState(null);
+
+  const apiKey = "fca_live_dxIYuhTXOQFtljZCfSXinVRkl11qqPzSwTh9HNGP";
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]:
+        name === "amount" || name === "totalDays" ? valueAsNumber(e) : value,
+    });
+  };
 
   const valueAsNumber = (event) => {
     const value = event.target.value;
-    return value === '' ? '' : Number(value); // Parse value as a number or keep it empty if it's ''
+    return value === "" ? "" : Number(value); // Parse value as a number or keep it empty if it's ''
   };
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData)
-    calculatedDailyBudget()
-    setShowDailyBudget(true)
+    setShowDailyBudget(true);
+    calculateAndSetDailyBudget();
   };
-    
 
-  const conversionRate = 2;
-  const dailyBudget = Math.round((formData.amount * conversionRate)/ formData.totalDays).toFixed(2)
-  
-  function calculatedDailyBudget(){
-    console.log(conversionRate);
-    console.log(dailyBudget)
+  const calculateAndSetDailyBudget = async () => {
+    try {
+      const rate = await makeApiCall(
+        apiKey,
+        formData.currency1,
+        formData.currency2
+      );
+      const totalBudget = (
+        (rate * formData.amount) /
+        formData.totalDays
+      ).toFixed(2);
+      setDailyBudget(totalBudget);
+    } catch (error) {
+      console.error("Error calculating daily budget:", error);
+    }
+  };
 
-  }
-
-  
   return (
     <>
       {/* <h1>Currency Conversion Form to go here</h1> */}
       <div className="formContainer">
-      <form onSubmit={handleSubmit} >
-      
-        <label>Choose your base Currency:</label>
-        <select id="currency1" name="currency1" value={formData.currency1} onChange={handleInputChange} >
-          <option value="GBP">GBP</option>
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="CAD">CAD</option>
-        </select>
-      
-        <label>Choose your Second Currency:</label>
-        <select id="currency2" name="currency2" value={formData.currency2} onChange={handleInputChange}>
-          <option value="GBP">GBP</option>
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="CAD">CAD</option>
-        </select>
+        <form onSubmit={handleSubmit}>
+          <label>Choose your base Currency:</label>
+          <select
+            id="currency1"
+            name="currency1"
+            value={formData.currency1}
+            onChange={handleInputChange}
+          >
+            <option value="GBP">GBP</option>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="CAD">CAD</option>
+            <option value="JPY">JPY</option>
+            <option value="INR">INR</option>
+            <option value="THB">THB</option>
+            <option value="DKK">DKK</option>
+            <option value="CHF">CHF</option>
+            <option value="HKD">HKD</option>
+          </select>
 
-        <label>Please enter amount:</label>
-        <input type="number" min={"1"} name="amount" value={formData.amount} onChange={handleInputChange}></input>
+          <label>Choose your Second Currency:</label>
+          <select
+            id="currency2"
+            name="currency2"
+            value={formData.currency2}
+            onChange={handleInputChange}
+          >
+            <option value="GBP">GBP</option>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="CAD">CAD</option>
+            <option value="JPY">JPY</option>
+            <option value="INR">INR</option>
+            <option value="THB">THB</option>
+            <option value="DKK">DKK</option>
+            <option value="CHF">CHF</option>
+            <option value="HKD">HKD</option>
+          </select>
 
-      <label>Length of trip (in days):</label>
-        <input type= "number" min={"1"} name="totalDays" value ={formData.totalDays} onChange={handleInputChange} ></input>
-        <input type="submit" value="Submit" className="submitButton"></input>
-        
-      </form>
+          <label>Please enter amount you wish to convert:</label>
+          <input
+            type="number"
+            min={"1"}
+            name="amount"
+            value={formData.amount}
+            onChange={handleInputChange}
+          ></input>
+
+          <label>Length of trip (in days):</label>
+          <input
+            type="number"
+            min={"1"}
+            name="totalDays"
+            value={formData.totalDays}
+            onChange={handleInputChange}
+          ></input>
+          <input type="submit" value="Submit" className="submitButton"></input>
+        </form>
       </div>
-      
-      {showDailyBudget && (
+
+      {showDailyBudget && dailyBudget !== null && (
         <div>
-          <h2>Your daily budget is {dailyBudget} {formData.currency2}</h2>
-        </div>)}
-        
+          <h2>
+            Your daily budget is {dailyBudget} {formData.currency2}.
+          </h2>
+          <h3>Have a wonderful trip and safe travels!</h3>
+        </div>
+      )}
     </>
-  )
+  );
 };
-
-
 
 export default CurrencyForm;
