f6fc1157043326c85c0e2c0880ffec6ed13e65db
Penny <pennyluc321@gmail.com>
2023-12-11 13:48:11 +0000

    renamed images to resolve conflicts of the imports. Changed the footer so it is in a fixed position

--- budget_planner/src/pages/InspirePage.js
+++ budget_planner/src/pages/InspirePage.js
@@ -1,8 +1,8 @@
 import Footer from "../components/Footer"
 import Heading from "../components/Heading"
-import image from "../Images/dubai.jpeg";
-import image from "../Images/Thailand.jpeg";
-import image from "../Images/Bali.jpeg";
+import dubai from "../Images/dubai.jpeg";
+import thailand from "../Images/Thailand.jpeg";
+import bali from "../Images/Bali.jpeg";
 
 
 function InspirePage() {
@@ -13,21 +13,21 @@ function InspirePage() {
         <h1>Inspiring Holiday Destinations</h1>
   
   <div class="destination">
-    <img src="./Images/dubai.jpeg" alt="Dubai"/>
+    <img src={dubai} alt="Dubai"/>
     <h2>Dubai</h2>
     <p>Experience luxury and extravagance in the heart of the desert.</p>
     <h3>Currency Conversion:</h3>
   </div>
   
   <div class="destination">
-    <img src="./Images/Thailand.jpeg" alt="Thailand"/>
+    <img src={thailand}alt="Thailand"/>
     <h2>Thailand</h2>
     <p>Discover breathtaking beaches and vibrant cultural experiences.</p>
     <h3>Currency Conversion:</h3>
   </div>
   
   <div class="destination">
-    <img src="Bali.jpeg" alt="Bali"/>
+    <img src={bali} alt="Bali"/>
     <h2>Bali</h2>
     <p>Immerse yourself in stunning landscapes and tranquil spirituality.</p>
     <h3>Currency Conversion:</h3>
