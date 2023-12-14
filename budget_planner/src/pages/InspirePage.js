a0b31d5c57edb44db8decd406b574fda9c39d567
Maryam Asaria <maryam.asaria@gmail.com>
2023-12-14 01:06:46 +0000

    Improved design of inspirePage by centering images and making them all the same size

--- budget_planner/src/pages/InspirePage.js
+++ budget_planner/src/pages/InspirePage.js
@@ -6,37 +6,41 @@ import bali from "../Images/Bali.jpeg";
 import "./InspirePage.css"
 
 
-function InspirePage() {
+const InspirePage = () => {
+  return (
+    <>
+      <Heading Heading="Holiday Inspirations"></Heading>
 
-    return (
-        <>
-        <Heading Heading="Holiday Inspirations"></Heading>
-        <h1 className="heading">Inspiring Holiday Destinations</h1>
-  
-  <div class="destination">
-    <img src={dubai} alt="Dubai"/>
-    <h2>Dubai</h2>
-    <p>Experience luxury and extravagance in the heart of the desert.</p>
-    <h3>Currency Conversion:</h3>
-  </div>
-  
-  <div class="destination">
-    <img src={thailand}alt="Thailand"/>
-    <h2>Thailand</h2>
-    <p>Discover breathtaking beaches and vibrant cultural experiences.</p>
-    <h3>Currency Conversion:</h3>
-  </div>
-  
-  <div class="destination">
-    <img src={bali} alt="Bali"/>
-    <h2>Bali</h2>
-    <p>Immerse yourself in stunning landscapes and tranquil spirituality.</p>
-    <h3>Currency Conversion:</h3>
-  </div>
-  
-        <Footer></Footer>
+      <div className="sub-heading">
+        <h1>Inspiring Holiday Destinations</h1>
+      </div>
+
+      <div className="row">
+
+      <div className="column">
+        <img src={dubai} alt="Dubai" className="destination-image" />
+        <h2>Dubai</h2>
+        <p>Experience luxury and extravagance in the heart of the desert.</p>
+        <h3>Currency Conversion:</h3>
+      </div>
+
+      <div className="column">
+        <img src={thailand} alt="Thailand" className="destination-image" />
+        <h2>Thailand</h2>
+        <p>Discover breathtaking beaches and vibrant cultural experiences.</p>
+        <h3>Currency Conversion:</h3>
+      </div>
+
+      <div className="column">
+        <img src={bali} alt="Bali" className="destination-image" />
+        <h2>Bali</h2>
+        <p>Immerse yourself in stunning landscapes and tranquil spirituality.</p>
+        <h3>Currency Conversion:</h3>
+      </div>
+    </div>
+      <Footer></Footer>
     </>
-    );
+  );
 }
 
 export default InspirePage;
