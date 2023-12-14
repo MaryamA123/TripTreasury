0701e91e9c5102c8ebf48e81351a63cf17f56e2f
Maryam Asaria <maryam.asaria@gmail.com>
2023-12-14 00:10:32 +0000

    added more design elements to the about us page

--- budget_planner/src/pages/AboutUsPage.js
+++ budget_planner/src/pages/AboutUsPage.js
@@ -4,40 +4,64 @@ import image from "../Images/pineapples.webp";
 import Heading from "../components/Heading";
 import './AboutUsPage.css';
 
-
 const AboutUsPage = () => {
-    return (
+  return (
 
-<>
+    <>
 
-        <Heading Heading="About us"/>
+      <Heading Heading="About us" />
 
-        <div className="content">
-     
+      <div className="intro-section">
         <h2>
-          Welcome to Trip Treasury, where budgeting for your dream holiday is done for you!
+          Welcome to Trip Treasury
         </h2>
-
-        <p>
+        <h2>
+          Where budgeting for your dream holiday is done for you!
+        </h2>
+        <h3>
           We are a dynamic group of individuals passionate about travel and helping others make the most of their vacations.
+          <br/><br/>
           Allow us to introduce ourselves:
-        </p>
+        </h3>
+      </div>
+      <br/>
+      
+      <img src={image} alt="pineappleimage" className="pineapple-image" />
+      
+      <br/>
 
-        <img src={image} alt="pineappleimage" className="pineappleimage"/>
+      <div className="row">
+        
+        <div className="column">
+          <div className="card">
+            <h2>Penny</h2> 
+            <h3>The crochet connoisseur</h3>
+          </div>
+        </div>
 
-        <p>
-          Penny - The crochet connoisseur
-          <br></br>
-          <br></br>
-          Asia - The skating sensation
-          <br></br>
-          <br></br>
-          Naomi - The pottery pro
-          <br></br>
-          <br></br>
-          Maryam - Our football enthusiast
-          </p>
+        <div className = "column">
+          <div className="card">
+              <h2>Asia</h2>
+              <h3>The skating sensation</h3>
+          </div>
+        </div>
 
+        <div className = "column">
+          <div className="card">
+              <h2> Naomi </h2>
+              <h3>The pottery pro </h3>
+          </div>
+        </div>
+
+        <div className = "column">
+          <div className="card">
+              <h2>Maryam</h2> 
+              <h3>Our football enthusiast</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="ending">
         <p>
           Our diverse interests come together to form the heart of Trip Treasury. We believe in the power of combining
           different passions to create something extraordinary, and that's exactly what we've done with this application.
@@ -59,12 +83,11 @@ const AboutUsPage = () => {
           Join us on this exciting journey, and let Trip Treasury be your trusted companion for budgeting on your next
           adventure!
         </p>
-        </div>
-        
+      </div>
 
-        <Footer/>
-        </>
-    );
-  };
-  
-  export default AboutUsPage;
+      <Footer />
+    </>
+  );
+};
+
+export default AboutUsPage;
